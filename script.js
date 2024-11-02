5250745c7b31ba3d43d11b49b0f1faa8c5193c67
Laure Maffini <lauremaffini@gmail.com>
2024-11-02 20:56:39 +0100

    [feat] : remove feat working

--- script.js
+++ script.js
@@ -2,21 +2,44 @@
 //////////Fonction pour récupérer l'input au clic sur le bouton//////////
 const addTaskSubmitBtn = document.querySelector("#taskSubmitBtn");
 const tasksArray = [];
-const taskList = document.querySelector("#tasksList")
-const input = document.querySelector("#input");
+const taskList = document.querySelector("#toDoList")
+const todoInput = document.querySelector("#todoInput");
 
 function addTask(task) {
-    tasksArray.push(input.value);
+    tasksArray.push(todoInput.value);
     const newLi = document.createElement("li");
+    newLi.innerText = todoInput.value ;
+
     const newBtn = document.createElement("button");
-    newLi.innerText = input.value ;
-    taskList.append(newLi, newBtn);
-    input.value = "";
+    newBtn.classList = "remove";
+    newBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+
+    newLi.appendChild(newBtn);
+    taskList.append(newLi);
+
+    todoInput.value = "";
 }
 
 addTaskSubmitBtn.addEventListener('click', (event) => {
     event.preventDefault();
-    addTask(input.value);
+    addTask(todoInput.value);
 }
 )
 
+//////////Fonction pour supprimer le li au clic sur le BinBtn//////////
+
+const removeTodo = (event) => {
+    const todoInput = event.target;
+    const liToremove= todoInput.closest("li");
+    const arrayContent = liToremove.innerText;
+
+    const arrayContentIndex = tasksArray.indexOf(arrayContent);
+    tasksArray.splice(arrayContentIndex,1);
+
+    if (todoInput.classList.contains("remove") || todoInput.closest(".remove")) {
+        liToremove.remove();
+    }
+
+};
+
+taskList.addEventListener('click', removeTodo);
